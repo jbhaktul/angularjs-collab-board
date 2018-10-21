d0967f575d8610c76252f67584c429ecf13e4ec8
John Bhaktul <john.bhaktul@gmail.com>
2018-10-20 21:55:53 -0400

    Updating lots of code.

--- app.js
+++ app.js
@@ -1,30 +1,19 @@
-var express = require('express'),
-	app = express(),
-	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
-	var path = require('path');
-	
-	app.set('port', process.env.PORT || 3000);
-	app.use(express.static(path.join(__dirname, 'public')));
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
 
 io.sockets.on('connection', function(socket) {
-	socket.on('createNote', function(data) {
-		socket.broadcast.emit('onNoteCreated', data);
-	});
+	socket.on('createNote', (data) => socket.broadcast.emit('onNoteCreated', data));
 
-	socket.on('updateNote', function(data) {
-		socket.broadcast.emit('onNoteUpdated', data);
-	});
+	socket.on('updateNote', (data) => socket.broadcast.emit('onNoteUpdated', data));
 
-	socket.on('moveNote', function(data){
-		socket.broadcast.emit('onNoteMoved', data);
-	});
+	socket.on('moveNote', (data) => socket.broadcast.emit('onNoteMoved', data));
 
-	socket.on('deleteNote', function(data){
-		socket.broadcast.emit('onNoteDeleted', data);
-	});
+	socket.on('deleteNote', (data) => socket.broadcast.emit('onNoteDeleted', data));
 });
 
-server.listen(app.get('port'), function () {
-	console.log('Express server listening on port ' + app.get('port'))
-});
+http.listen(port, () => console.log('listening on port ' + port));
